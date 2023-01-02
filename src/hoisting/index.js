b4772ab5414b8091dbd8185fd970e3dabb8e5f87
Mauro Caffesse <mscaffesse@gmail.com>
2023-01-02 19:38:14 -0300

    Commit Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,14 @@
+// var nameOfDog = 'LeBron' // Undefined
+
+// console.log(nameOfDog);
+// var nameOfDog = 'LeBron';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${LeBron}`);
+}
+
+var Lebron = 'LeBroncito';
+//Al ejecutar la función, figura 'el mejor perrito es undefined', porque el elemento var se elevó.
